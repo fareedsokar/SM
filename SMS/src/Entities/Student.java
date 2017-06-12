e091e70916ebc6ab495fe3d5129f693acfc284c9
DESKTOP-H5C8QMA\Sally <sallydabbah@gmail.com>
2017-06-12 16:06:01 +0300

    Changed Student Constructor

--- SMS/src/Entities/Student.java
+++ SMS/src/Entities/Student.java
@@ -1,10 +1,14 @@
 package Entities;
 
-public class Student {
+public class Student extends User{
 
+	public Student(int ID, String Name, String Password, String Email, int Status, AccessProfiles Access, Parent parent[]) {
+		super(ID, Name, Password, Email, Status, Access);
+		setParent(parent);
+		// TODO Auto-generated constructor stub
+	}
 	private Parent parent[];
 	
-
 	public Parent[] getParent() {return parent;}
 	public void setParent(Parent parent[]) {this.parent = parent;}
 }
